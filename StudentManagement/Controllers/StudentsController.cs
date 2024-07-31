73fea48fe2043de85029e3b701258943189c5161
Leonardo Figueiredo <leofigueiredo00@gmail.com>
2024-07-31 12:28:29 -0400

    initial commit

---
+++ StudentManagement/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+using StudentManagement.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.students.ToListAsync());
+        }
+        // Other actions (Create, Edit, Delete) can be added here
+    }
+}
---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string course { get; set; }
+    }
+}
